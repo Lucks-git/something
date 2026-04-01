1e3c60a9fa2eef1543c39145a4605838244a252e
Lu.cks <112194186+Lucks-git@users.noreply.github.com>
2026-04-01 11:05:21 +0000

    Main change

--- Main.js
+++ Main.js
@@ -1,1 +1,16 @@
+window.addEventListener("load", function () {
+    const loader = document.getElementById("loader");
+    const button = document.getElementById("MInfo");
 
+    loader.classList.add("hidden");
+    
+    console.log("button:", button);
+
+    button.onclick = function (){
+
+        button.classList.toggle("active")
+
+        console.log(button)
+
+    }
+}); 
